b607f289be0c5b0da94567e567c1b877bbff8436
Dorian Blanc <Dorian Blanc>
2016-03-10 16:46:33 +0100

    site

--- Site/report_site/report.js
+++ Site/report_site/report.js
@@ -1,29 +1,41 @@
-var report = "<?xml version='1.0' encoding='UTF-8' standalone='no'?> \
-<mutations name='Projet Salade de fruit'> \
-  <mutation name='MutationFraise'>\
-    <class name='FooTest' originalPath='../../src/main/java/Foo.java' mutantPath='../generated-sources/spoon/Foo.java'>\
-      <method name='plusTest' status='alive'/>\
-      <method name='multTest' status='alive'/>\
-      <method name='minusTest' status='alive'/>\
+var report = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\
+<mutations>\
+  <mutation name="EqualsProcessor">\
+    <class name="FooTest" aliveMutant="2" deadMutant="1">\
+      <codeOriginal></codeOriginal>\
+      <codeMutant></codeMutant>\
+      <codeTest></codeTest>\
+      <method name="plusTest" status="dead" time="0.001"/>\
+      <method name="multTest" status="alive" time="0"/>\
+      <method name="minusTest" status="alive" time="0"/>\
     </class>\
-    <class name='TriangleTest' originalPath='../../src/main/java/Triangle.java' mutantPath='../generated-sources/spoon/Triangle.java'>\
-      <method name='testEquilateral' status='dead'/>\
-      <method name='testIsoceles' status='dead'/>\
-      <method name='testScalene' status='dead'/>\
-      <method name='testGiantTriangle' status='alive'/>\
-    </class>\
-  </mutation>\
-  <mutation name='MutationPomme'>\
-    <class name='FooTest' originalPath='../../src/main/java/Foo.java' mutantPath='../generated-sources/spoon/Foo.java'>\
-      <method name='plusTest' status='dead'/>\
-      <method name='multTest' status='dead'/>\
-      <method name='minusTest' status='dead'/>\
-    </class>\
-    <class name='TriangleTest' originalPath='../../src/main/java/Triangle.java' mutantPath='../generated-sources/spoon/Triangle.java'>\
-      <method name='testEquilateral' status='dead'/>\
-      <method name='testIsoceles' status='dead'/>\
-      <method name='testScalene' status='dead'/>\
-      <method name='testGiantTriangle' status='alive'/>\
+    <class name="TriangleTest" aliveMutant="2" deadMutant="2" >\
+      <codeOriginal></codeOriginal>\
+      <codeMutant></codeMutant>\
+      <codeTest></codeTest>\
+      <method name="testEquilateral" status="dead" time="0"/>\
+      <method name="testIsoceles" status="alive" time="0"/>\
+      <method name="testScalene" status="dead" time="0"/>\
+      <method name="testGiantTriangle" status="alive" time="0"/>\
     </class>\
   </mutation>\
-</mutations>";
+  <mutation name="PlusProcessor">\
+    <class name="FooTest" aliveMutant="0" deadMutant="3">\
+      <codeOriginal></codeOriginal>\
+      <codeMutant></codeMutant>\
+      <codeTest></codeTest>\
+      <method name="plusTest" status="dead" time="0.001"/>\
+      <method name="multTest" status="dead" time="0"/>\
+      <method name="minusTest" status="dead" time="0"/>\
+    </class>\
+    <class name="TriangleTest" aliveMutant="1" deadMutant="3">\
+      <codeOriginal></codeOriginal>\
+      <codeMutant></codeMutant>\
+      <codeTest></codeTest>\
+      <method name="testEquilateral" status="dead" time="0"/>\
+      <method name="testIsoceles" status="alive" time="0"/>\
+      <method name="testScalene" status="dead" time="0"/>\
+      <method name="testGiantTriangle" status="dead" time="0"/>\
+    </class>\
+  </mutation>\
+</mutations>';
