d845f0afa6c72a80f213d78eef143316a3f0dba2
Manuel Pavone <pavone.pro@gmail.com>
2016-03-10 23:14:47 +0100

    Script final

--- start.sh
+++ start.sh
@@ -7,11 +7,18 @@ prec="<processor>.*<\/processor>"
 pom_path="."
 
 # Emplacement de l'html
-html_path="$pom_path\target\html-report"
+html_path="$pom_path\Site\report_site"
 
-report_final="report.js"
+report_final="$pom_path\Site\report_site\report.js"
+
+path_to_classes="$pom_path\src\main\java"
+
+path_to_mutants="$pom_path\src\main\java"
 
 tabchar="	"
+
+# variable
+actual_class=""
 
 
 > $report_final
@@ -27,18 +34,38 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	
 		sed -i -e "s/$prec/<processor>fr.unice.polytech.doct13.processors.$line<\/processor>/g" "$pom_path\pom.xml"
 		#je lance les tests
-		mvn test -f "$pom_path"
+		#mvn test -f "$pom_path"
 
-		testresult=`grep '<.*>' target/html-report/report.xml | sed -e "s/<?xml.*<mutation>/<mutation name=\"$line\">/"`
+		testresult=`grep '<.*>' target/html-report/report.xml | sed -e "s/<?xml.*<mutation name=\"\">/<mutation name=\"$line\">/"`
 
-		echo "$testresult" >> $report_final
 	fi
 
+	if [[ $line == *className* ]]
+	then
+
+		class_name=`echo "$line" | sed -e "s|$tabchar||g" -e "s| ||g" -e "s|\".*: *\"||g" -e "s|\",||g"`;
+
+		##old_code=`cat $path_to_classes/$class_name.java`
+		##old_code="<codeOriginal>$old_code</codeOriginal>"
+
+		##testresult1=`echo "$testresult" | sed "s/<method name=/<codeOriginal><\/codeOriginal><codeMutant><\/codeMutant><codeTest><\/codeTest><method name=/1"`
+		##testresult1=`echo "$testresult1" | sed "s/<\/codeOriginal>.*//1"`
+		##testresult2=`echo "$testresult" | sed "s/.*<codeOriginal>//1"`
+
+		#echo "$testresult1" >> $report_final
+		#echo "$old_code" >> $report_final
+		#echo "$testresult2" >> $report_final
+
+		echo "$testresult" >> $report_final
+
+	fi
 
 done < "$1"
 
-echo "</mutations>'" >> $report_final
-
 sed -i -e "s/$/\\\/g" -e "s/<\/mutations>'\\\/<\/mutations>/" $report_final
+
+echo "</mutations>';" >> $report_final
+
+
 
 #"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "$html_path\index.html"
